fe048b205c977d0778c87132002df473942037ef
bolodissenoura <danielunivap@outlook.com>
2024-06-13 14:58:39 -0300

    feat: testing permissions on api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,10 @@
-console.log('sexo')
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
